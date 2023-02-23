10c317183ca9cdd704415144e780530eb37f9b2c
Keith Baston <keith.baston@gmail.com>
2023-02-23 15:50:59 -0800

    added plumbing for speech.

--- fs.py
+++ fs.py
@@ -1,7 +1,19 @@
+import asyncio
+
+import pyttsx3
+
 from textual.app import App, ComposeResult
 from textual.containers import Container
+from textual.message import Message, MessageTarget
 from textual.reactive import reactive
 from textual.widgets import Button, Header, Footer, Static
+
+
+class Say(Message):
+    def __init__(self, sender: MessageTarget, text: str, queue=False) -> None:
+        self.text = text
+        self.queue = queue
+        super().__init__(sender)
 
 
 class ResultDisplay(Static):
@@ -13,6 +25,11 @@ class WordDisplay(Static):
 
 
 class WordTry(Static):
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        button_id = event.button.id
+        if button_id == "say":
+            await self.post_message(Say(self, "Do it!"))
+
     def compose(self) -> ComposeResult:
         yield WordDisplay("hel_", id="word_display")
         yield ResultDisplay("123", id="results")
@@ -26,15 +43,28 @@ class FansticSpellingApp(App):
     CSS_PATH = "fs.css"
     BINDINGS = []
 
+    async def on_mount(self) -> None:
+        self.tts = pyttsx3.init()
+        self.tts.startLoop(False)
+
+        async def pump_tts():
+            while True:
+                if self.tts.isBusy():
+                    self.tts.iterate()
+                await asyncio.sleep(0)
+
+        asyncio.create_task(pump_tts())
+
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
         yield Header()
         yield Footer()
         yield Container(WordTry())
 
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
+    def on_say(self, message: Say) -> None:
+        if self.tts.isBusy() and not message.queue:
+            return
+        self.tts.say(message.text)
 
 
 if __name__ == "__main__":
